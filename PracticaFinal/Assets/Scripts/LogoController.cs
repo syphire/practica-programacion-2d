497209d7791efbdb5ab3750f4e3be7c8e3f4e9cf
Silvia Piñeiro <silvia@silviapineiro.es>
2021-01-10 18:25:05 +0100

    Configurar pantallas de logo, menú y opciones

---
+++ PracticaFinal/Assets/Scripts/LogoController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogoController : MonoBehaviour
+{
+    public float delay = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("TitlePage");
+    }
+}
---
+++ PracticaFinal/Assets/Scripts/MenuPrincipalController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuPrincipalController : MonoBehaviour
+{
+    private string buttonClicked;
+
+    // Use this for initialization
+    void Start()
+    {
+        /*NuevaPartida.GetComponent<Button>();
+        Controles.GetComponent<Button>();
+        Salir.GetComponent<Button>();*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+
+    public void ButtonClick()
+    {
+        buttonClicked = EventSystem.current.currentSelectedGameObject.name;
+
+        if (buttonClicked == "EmpezarPartidaButton")
+        {
+            StartGame();
+        }
+        else if (buttonClicked == "ControlesButton")
+        {
+            ReadControles();
+        }
+        else if (buttonClicked == "SalirButton")
+        {
+            Exit();
+        }
+        else if (buttonClicked == "AtrasButton")
+        {
+            Debug.Log("boton atras");
+            GoBack();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameFirstLevel");
+    }
+
+    public void ReadControles()
+    {
+        SceneManager.LoadScene("InfoControles");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        Debug.Log("dentro del boton atras");
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+
+}
